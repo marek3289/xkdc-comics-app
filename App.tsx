9a08784d2fc1adc4dee15c7394e03bd076920242
marek3289 <marek2494@o2.pl>
2020-07-15 14:56:38 +0200

    deleted theme provider

--- App.tsx
+++ App.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ThemeProvider } from 'styled-components/native';
 
 import Navigation from './navigation';
 import { Header } from './components';
-import { theme } from './styles';
 
 const App = () => {
-  console.log(theme)
 
   return (
     <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <Header />
-        <Navigation />
-        <StatusBar />
-      </ThemeProvider>
+      <Header />
+      <Navigation />
+      <StatusBar />
     </SafeAreaProvider>
   );
 };
